4fbbd1211681b1e875d0fafc02b8ae6d650ed8bf
Dhruv <chaudharidhruv997@gmail.com>
2026-02-19 00:52:13 +0530

    Django project setup with templates and static files

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,20 @@
-from django.http import HttpResponse
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
 def home(request):
-    return HttpResponse("Hello World");
+    # return HttpResponse("Hello World")
+    return render(request, 'calc.html')
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def add(request):
+    val1 = request.POST['num1']
+    val2 = request.POST['num2']
+    res = int(val1) + int(val2)
+    return render(request, 'result.html', {'result': res})
